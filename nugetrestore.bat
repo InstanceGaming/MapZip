473ec0d3977ee2f2d7d089d61aeee6b6286c8cea
InstanceGaming <jacoblj3333@gmail.com>
2018-01-16 09:42:20 -0700

    Fixed incorrect paths in nugetrestore.bat

--- nugetrestore.bat
+++ nugetrestore.bat
@@ -1,9 +1,15 @@
 @echo off
 SET packagePath=%~dp0Installer\packages\Newtonsoft.Json.10.0.3\
 SET webAddress=http://packages.nuget.org/api/v1/package/Newtonsoft.Json/
+if not exist "%packagePath%" ( goto :getpkgs )
+
+:getpkgs
 echo Getting NuGet package(s) from remote.
-mkdir %packagePath%
+mkdir "%packagePath%"
 powershell -Command "(New-Object Net.WebClient).DownloadFile('%webAddress%', '%~dp0package.zip')"
-powershell -Command "Invoke-WebRequest %webAddress% -OutFile %~dp0package.zip"
-%~dp0Dist\7z\7za.exe x %~dp0package.zip -o%packagePath% -r
-del /s /q %~dp0package.zip 
+powershell -Command "Invoke-WebRequest '%webAddress%' -OutFile '%~dp0package.zip'"
+cd "%~dp0Dist\7z\"
+7za.exe x "%~dp0package.zip" -o"%packagePath%" -r
+del /s /q "%~dp0package.zip"
+
+pause
